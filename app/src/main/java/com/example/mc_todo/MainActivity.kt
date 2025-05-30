29b4cab0cfc298013941ed4fecc7224c744e857b
Emmanuel3Contreras <147527317+Emmanuel3Contreras@users.noreply.github.com>
2025-05-30 14:28:02 -0600

    Errores de compatibilidad solucionados

--- app/src/main/java/com/example/mc_todo/MainActivity.kt
+++ app/src/main/java/com/example/mc_todo/MainActivity.kt
@@ -4,21 +4,10 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-feature/login
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.google.firebase.Firebase
-import com.google.firebase.initialize
-
-master
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-feature/login
-        Firebase.initialize(this)
-        enableEdgeToEdge()
- master
         setContentView(R.layout.activity_main)
 
         // Encuentra el botón por su ID
@@ -27,8 +16,6 @@ feature/login
         // Configura el Listener de clic para el botón
         goToTasksButton.setOnClickListener {
             // Crea un Intent para iniciar TaskListActivity
-            // TaskListActivity aún no existe, la crearemos en el siguiente paso.
-            // Por ahora, el IDE puede marcar un error aquí, lo cual es normal.
             val intent = Intent(this, TaskListActivity::class.java)
             startActivity(intent) // Inicia la TaskListActivity
         }
